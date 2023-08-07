3763ae3e45bc7d60af08586fa8928e1d6c442fe5
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-08-06 21:23:48 -0300

    Ajustes

---
+++ Aula_06/aula-06.py
@@ -0,0 +1,10 @@
+curso = "Curso de Python "
+
+# print(curso[9:15])
+# print(curso.strip())
+# print(curso.lower().strip())
+# print(curso.upper().strip())
+# print(curso.replace("Python", "C#"))
+a = curso.split(". ")
+print(a[0])
+print("Tamanho: " + str(len(curso)))
---
+++ Aula_07/aula_07.py
@@ -0,0 +1,45 @@
+cidade = "Artur Nogueira"
+dia = 22
+mes = "Julho"
+ano = 2023
+canal = "CFB Cursos"
+data = f"{cidade}, {dia} de {mes} de {ano} \n \"{canal}\""
+
+
+print(data)
+
+
+# curso = "Curso de Python "
+# canal = "CFB Cursos"
+
+# res = curso + "do canal " + canal
+
+# print(res)
+
+
+
+
+
+
+
+
+
+# res = palavra.upper() in texto.upper()
+# print(res)
+
+
+
+
+
+
+
+
+
+# print(curso[9:15])
+# print(curso.strip())
+# print(curso.lower().strip())
+# print(curso.upper().strip())
+# print(curso.replace("Python", "C#"))
+# a = curso.split(". ")
+# print(a[0])
+# print("Tamanho: " + str(len(curso)))
