46036a328a7659192acc795e362033d91696ffba
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-08-20 22:00:42 -0300

    Create aula-08.py

---
+++ Aula_08/aula-08.py
@@ -0,0 +1,15 @@
+# aula = 10 > 15
+
+aula = "CFB Cursos"
+
+print(bool (aula))
+
+
+
+# if aula:
+#     print("Possui texto")
+# else:
+#     print("Não possui texto")
+
+# print(bool (aula))
+# # Aqui irá conseguir saber se tem texto ou não na variavel com verdadeiro ou falso, excelente para procurar senhas
