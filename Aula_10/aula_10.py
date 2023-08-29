fe2f9c3351f20ee5773889b8bcc2aa6ad364528d
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-08-28 21:39:10 -0300

    Aula 10 cfbcursos

--- Aula_10/aula_10.py
+++ Aula_10/aula_10.py
@@ -1,1 +1,18 @@
-a = True
+a = 10
+b = 5
+res = 0
+op = "*"
+
+if op == "+":
+    res = a+b
+
+if op == "-":
+    res = a-b
+
+if op == "*":
+    res = a*b
+
+if op == "/":
+    res = a/b 
+
+print(str(a) + op + str(b) + " = " + str(res));
