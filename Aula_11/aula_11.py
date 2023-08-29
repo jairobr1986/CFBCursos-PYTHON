3971e139167e1a2f84ba707e233b1264b19b235d
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-08-28 23:01:06 -0300

    Aula 11 sem terminar

---
+++ Aula_11/aula_11.py
@@ -0,0 +1,40 @@
+clima = "chuva"
+lugar = ""
+
+if clima == "Sol":
+    lugar = "Clube"
+else:
+    lugar = "Cinema"
+
+print("Vou ao " + lugar)
+
+
+
+
+
+
+
+
+
+
+# a = 10
+# b = 5
+# res = 0
+# op = "*"
+
+# if op == "+":
+#     res = a+b
+
+# elif op == "-":
+#     res = a-b
+
+# elif op == "*":
+#     res = a*b
+
+# elif op == "/":
+#     res = a/b 
+
+# else:
+#     print("Operador Invalido")
+
+# print(str(a) + op + str(b) + " = " + str(res));
