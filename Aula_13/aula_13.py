abe0dc692be5bd118010de542e4eff8c8a86ae6e
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-09-01 22:45:00 -0300

    aula 13 ok

---
+++ Aula_13/aula_13.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+# nome = input("Digite seu nome: ")
+# print("Nome digitado: " + nome)
+
+num1 = int(input("Digite o primeiro valor..: "))
+num2 = int(input("Digite o segundo valor..: "))
+res = num1 + num2
+print("Soma do valores: " + str(res))
